61376deb680f54ff9e3eaa43297cdc2d4c201a0b
gpmmarketing <122981953+gpmmarketing@users.noreply.github.com>
2023-07-17 11:02:20 -0400

    Updated Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
+        <Text variant="h1">Answers to Anything: The Revolutionary AI Platform That Knows No Boundaries</Text>
         <Text className="text-zinc-600">
           In this example, a simple chat bot is implemented using Next.js, API
           Routes, and OpenAI API.
@@ -13,7 +13,7 @@ function Home() {
       </section>
 
       <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
+        <Text variant="h2">Type your question below:</Text>
         <div className="lg:w-2/3">
           <Chat />
         </div>
